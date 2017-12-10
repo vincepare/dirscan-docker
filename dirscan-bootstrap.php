cc854f25ce4c06ed79a864efb07ed2909e4a6539
Vincent Paré <vincent.pare@gmail.com>
2017-12-10 15:50:12 +0100

    Fix chrooted environnement magic
    Temporary copying dirscan inside the chroot to avoid having dirscan
    permanently available inside it.

--- dirscan-bootstrap.php
+++ dirscan-bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * dirscan-docker bootstrap
  *
- * Some piece of black magic to make dirscan working on the docker host from its container.
+ * Some piece of black magic to make dirscan <= 1.2 working on the docker host from its container.
  *
  * @author Vincent Paré
  * @license http://opensource.org/licenses/Apache-2.0
@@ -13,6 +13,12 @@ if (!is_dir('/hostfs')) {
     fwrite(STDERR, 'Please mount / on /hostfs to let dirscan explore the host filesystem (ex: -v "/":"/hostfs:ro").' . "\n");
     die(1);
 }
+
+// Copying dirscan inside chroot + end cleanup
+exec('cp -pv /tmp/.dirscan/dirscan /hostfs/tmp/.dirscan/dirscan');
+register_shutdown_function(function () {
+    unlink('/tmp/.dirscan/dirscan');
+});
 
 // chroot to host's filesystem
 chroot('/hostfs');
